c5971c87bd79ff777ae02b653124bf18ec00aed4
Clément Ruiz <clement.ruiz@ynov.com>
2017-03-11 14:56:34 +0100

    Continuation des modèles User et Commentaire

--- app/User.php
+++ app/User.php
@@ -9,25 +9,18 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = array('nom', 'prenom', 'email', 'login', 'password' );
+    protected $hidden = array('password', 'remember_token');
+    public static $rules = array(
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-    public function notables() //relation n/n
+    );
+
+    public function commentaires()
     {
-        return $this->belongsToMany('App\Notable', 'user_notable', 'user_id', 'notable_id')->withPivot('note');
+        return $this->hasMany("App\Commentaire", "user_id", "id");
+    }
+
+    public function tags(){
+        return $this->belongsToMany("App\Tag", "user_tag", "user_id", "tag_id");
     }
 }
