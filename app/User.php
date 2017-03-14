d69e7255c146faa59e1ffa3859e211e34defc924
Clément Ruiz <clement.ruiz@ynov.com>
2017-03-14 17:12:29 +0100

    continuing User Model (work in progress)x

--- app/User.php
+++ app/User.php
@@ -8,8 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
-    protected $fillable = array('nom', 'prenom', 'email', 'login', 'password' );
+    protected $table = "users";
+    protected $fillable = array('nom', 'prenom', 'email', 'login', 'password', 'birth_date', 'type', 'avatar' );
     protected $hidden = array('password', 'remember_token');
     public static $rules = array(
         "create" => array(
@@ -17,14 +17,17 @@ class User extends Authenticatable
             "prenom" => "required|string|min:2",
             'email' => 'required|email|unique:users',
             "login" => 'required|string|min:2|unique:users',
-            'password' => 'required|string|min:2'
+            'password' => 'required|string|min:8',
+            'birth_date' => 'string',
+            'type' => 'required|string',
+            'avatar' => 'url'
         ),
         "update" => array(
             "nom" => "string|min:2",
             "prenom" => "string|min:2",
             'email' => 'email|unique:users',
-            "login" => 'required|string|min:2|unique:users',
-            'password' => 'required|string|min:2'
+            'password' => 'string|min:8',
+            'birth_date' => 'string|min:5'
         )
     );
 
